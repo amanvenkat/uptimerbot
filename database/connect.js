cc4067d9e755b630a471696aacb7266f6b461a32
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-09-29 23:01:40 +0530

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connect() {
-  mongoose.connect(process.env.MOGO_URI, {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
