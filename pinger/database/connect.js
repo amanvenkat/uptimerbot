e3ab2bade9e9a58a3d3445d3b861abaa63fe0036
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-09-29 22:25:37 +0530

    Update connect.js

--- pinger/database/connect.js
+++ pinger/database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    mongoose.connect(`mongodb+srv://test:test@cluster0.p5eht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
